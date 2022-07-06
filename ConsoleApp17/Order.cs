ce7908bf770fce4c83cac3388e9f82796d8c680c
Jonika <katyadyduch@gmail.com>
2022-07-06 17:26:30 +0300

    ddd

---
+++ ConsoleApp17/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Order
+    {
+        private int _id;
+        List<Flower> _flowers = new List<Flower>();
+        bool status;
+        public int id
+        { get { return id; } set { id = value; } }
+        public List<Flower> flowers
+        { get { return _flowers; } set { _flowers = value; } }
+        Order(List<Flower> f , int num)
+        {
+            status = false;
+            flowers = f;
+            id = num;
+        }
+    }
+}
