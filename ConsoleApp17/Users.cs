d6a88e28170f5f75afb679893c3d080ed83cae3a
Stol_in54 <Stol_in54@student.itstep.org>
2022-07-06 15:26:10 +0200

    add users

---
+++ ConsoleApp17/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        
+        public void Exit()
+        {
+            Console.WriteLine("Enter your username -> ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter your password -> ");
+            Password = Console.ReadLine();
+
+
+        }
+
+    }
+}
